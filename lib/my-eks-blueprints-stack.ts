69228e98c19072bc3c1133a5c13a1c54526dd0de
Blueprints User <blueprints.users@workshops.com>
2022-11-29 20:32:31 +0000

    Added ArgoCD

--- lib/my-eks-blueprints-stack.ts
+++ lib/my-eks-blueprints-stack.ts
@@ -4,7 +4,7 @@ import { Construct } from 'constructs';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
 
 import { TeamPlatform, TeamApplication } from '../teams'; 
-import { KyvernoAddOn } from './kyverno_addon'; // WE IMPORT THE ADDON HERE
+import { KyvernoAddOn } from './kyverno_addon';
 
 export default class ClusterConstruct extends Construct {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,14 +12,32 @@ export default class ClusterConstruct extends Construct {
 
     const account = props?.env?.account!;
     const region = props?.env?.region!;
+    
+    const repoUrl = 'https://github.com/aws-samples/eks-blueprints-workloads.git'
+  
+    // HERE WE ADD THE ARGOCD APP OF APPS REPO INFORMATION
+    const bootstrapRepo : blueprints.ApplicationRepository = {
+        repoUrl,
+        targetRevision: 'workshop',
+    }
+    
+    // HERE WE GENERATE THE ADDON CONFIGURATION
+    const bootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'envs/dev'
+        },
+    });
 
+    
     const blueprint = blueprints.EksBlueprint.builder()
     .account(account)
     .region(region)
     .addOns(
       new blueprints.ClusterAutoScalerAddOn,
-      new KyvernoAddOn, // HERE WE ONBOARD THE ADDON
-      )
+      new KyvernoAddOn(), 
+      bootstrapArgo // HERE WE ADD OUR NEW ADDON WITH THE CONFIGURED ARGO CONFIGURATIONS
+    ) 
     .teams(new TeamPlatform(account), new TeamApplication('burnham',account)) 
     .build(scope, id+'-stack');
   }
